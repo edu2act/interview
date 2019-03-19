5573234e42480af98febae0fff6a4c9fcc340ded
eetze <eetze@foxmail.com>
2019-03-19 17:28:57 +0800

    add ds-stack

--- gitbook/gitbook-plugin-insert-logo-link-style/plugin.js
+++ gitbook/gitbook-plugin-insert-logo-link-style/plugin.js
@@ -2,18 +2,20 @@ require(['gitbook', 'jQuery'], function (gitbook, $) {
   var src = ''
   var style = ''
   var link = ''
+  var target = ''
 
-  var insertLogo = function (src, style, link) {
-    $('.book-summary').children().eq(0).before('<a class="book-logo" target="_blank" href="' + link + '"><img src="' + src + '" style="' + style + '"></a>')
+  var insertLogo = function (src, style, link, target) {
+    $('.book-summary').children().eq(0).before('<a class="book-logo" ' + (target?'target="' + target + '" ':'') +'href="' + link + '"><img src="' + src + '" style="' + style + '"></a>')
   }
 
   gitbook.events.bind('start', function (e, config) {
     src = config['insert-logo-link-style']['src']
     style = config['insert-logo-link-style']['style']
     link = config['insert-logo-link-style']['link']
+    target = config['insert-logo-link-style']['target']
   })
 
   gitbook.events.bind("page.change", function() {
-    insertLogo(src, style, link)
+    insertLogo(src, style, link, target)
   })
 })
